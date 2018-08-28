6a67518321e1ca1883d5ee4deca6701d31deb795
ronnasayd <ronnasayd@hotmail.com>
2018-08-28 11:44:34 -0300

    add stop

--- djangodocker.sh
+++ djangodocker.sh
@@ -23,6 +23,12 @@ if [ "$1" = "--run" ];then
 	bash make_ambient.sh
 fi
 
+if [ "$1" = "--stop" ];then
+	docker stop $(docker ps -a -q)
+	docker system prune -f
+	echo "Containers limpos"
+fi
+
 if [ "$1" = "--clear" ];then
 	sudo rm -r ./databases ./logs ./media ./nginx ./__pycache__ ./static ./*.Dockerfile ./*.yml ./make_ambient.sh ./runserver.sh ./requirements.txt
 	echo "Ambiente limpo"
