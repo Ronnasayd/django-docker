1f8618dbc57d10fd92a3bd17cc1125fcbd288b85
ronnasayd <ronnasayd@hotmail.com>
2018-12-04 14:40:21 -0300

    update

--- modules/update.sh
+++ modules/update.sh
@@ -1,16 +1,29 @@
 if [ ! -d "tmp/" ]; then
   mkdir tmp/
 fi
-wget https://raw.githubusercontent.com/Ronnasayd/django-docker/master/modules/version -O tmp/version.txt
+wget -q https://raw.githubusercontent.com/Ronnasayd/django-docker/master/modules/version -O tmp/version
 
-if ! diff -q tmp/version.txt modules/version.txt > /dev/null 2>&1;
+if ! diff -q tmp/version modules/version > /dev/null 2>&1;
 then
 
-  echo "Files are changed"
+  echo -e "you have a update"
+  echo -e "do you want update ? (y/n):"
+  read -s answer
+
+  if [ "$answer" = "y" ];then
+  	echo "updating..."
+  	wget -q https://github.com/Ronnasayd/django-docker/blob/master/source_code.zip?raw=true -O tmp/source_code.zip
+  	unzip tmp/source_code.zip -d tmp/
+  	rm tmp/source_code.zip
+  	cp tmp/pydd.py .
+  	cp tmp/ddo.sh .
+  	cp -r tmp/modules .
+  fi
+
 
 else
 
-  echo "Files not changed"
+  echo "You already update !"
 
 fi
 
