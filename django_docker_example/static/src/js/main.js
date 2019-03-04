eee52cebe3941bf806007fc4040b3e54feb70e05
ronnasayd <ronnasayd@hotmail.com>
2019-03-04 12:18:42 -0300

    fix codacy

--- django_docker_example/static/src/js/main.js
+++ django_docker_example/static/src/js/main.js
@@ -5,9 +5,6 @@ hammertime.on("swipe", function(ev) {
   if (ev.offsetDirection === 2) {
     $("a[data-slide='next']").click();
   } else {
-    var django2json = function(string_object) {
-      return JSON.parse(string_object.split("&#39;").join('"'));
-    };
 
     $(".carousel").carousel();
     var hammertime = new Hammer($(".painel")[0], {});
