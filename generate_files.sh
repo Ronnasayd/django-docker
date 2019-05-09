714fe147cc264502773574489dd5f6c314e9a8eb
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-05-09 16:43:11 -0300

    add coverage

--- generate_files.sh
+++ generate_files.sh
@@ -13,4 +13,8 @@ zip django_docker -r modules/ pydd.py ddo.sh config.py
 wget -m -p -E -k http://localhost/
 mv ./localhost ./docs
 sed -i -e 's/index.html//g' docs/index.html
+coverage run pydd.py
+coverage xml
+export CODACY_PROJECT_TOKEN=493db568a4594312846b4f614f054705
+python-codacy-coverage -r coverage.xml
 python ddotests.py
